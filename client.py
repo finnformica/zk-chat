f697590e2a1fb1d141ac671c2b3a9ca0d7070993
finnformica <finnformica@gmail.com>
2023-07-21 19:01:36 +0700

    adapt client model for proxy server

--- client.py
+++ client.py
@@ -1,5 +1,3 @@
-import asyncio
-import aiohttp
 import functools
 import time
 
@@ -20,31 +18,23 @@ def timed(N, url, fn):
     return wrapper
 
 
-def get(url):
-    resp = requests.get(url)
-    assert resp.status_code == 200
+def post(url, worker_no):
+    resp = requests.post(
+        url, json={"msg": f"Hello chatbot! I am worker {worker_no}", "api_key": 123456}
+    )
+    assert resp.status_code == 200, print(f"Worker {worker_no} failed to process")
+
+    print(f"Worker {worker_no} finished processing")
     return resp.json()
-
-
-def sync_get_all(url, n):
-    l = [get(url) for _ in range(n)]
-    return l
-
-
-def run_bench(n, funcs, urls):
-    for url in urls:
-        for func in funcs:
-            timed(n, url, func)(url, n)
 
 
 def thread_pool(url, n, limit=None):
     limit_ = limit or n
     with Pool(max_workers=limit_) as pool:
-        result = pool.map(get, [url] * n)
+        result = pool.map(post, [url] * n, range(n))
     return result
 
 
 if __name__ == "__main__":
-    urls = ["http://127.0.0.1:8000/wait", "http://127.0.0.1:8000/asyncwait"]
-    funcs = [sync_get_all, thread_pool]
-    run_bench(1000, funcs, urls)
+    url = "http://localhost:10000/tps"
+    thread_pool(url, 10)
