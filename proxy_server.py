0fd778fcfde492e99de5e32c9e28e59403c228eb
finnformica <finnformica@gmail.com>
2023-07-21 19:01:49 +0700

    add pydantic data model

--- proxy_server.py
+++ proxy_server.py
@@ -1,20 +1,25 @@
+from pydantic import BaseModel
 from fastapi import FastAPI
 import requests
 
-app = FastAPI()
+app = FastAPI(
+    title="zkChat Proxy Server",
+    description="This is a proxy server for zkChat.",
+)
+
+
+class ProxyRequest(BaseModel):
+    msg: str
+    api_key: int
+
 
 tps_base_url = "http://localhost:10001"
 
 
-@app.get("/")
-def root():
-    return {"message": "zkChat Proxy Server"}
-
-
 @app.post("/tps")
-def query_tps(msg="Hello chatbot!", api_key=123456):
+def query_tps(req: ProxyRequest):
     res = requests.post(
         tps_base_url + "/chat",
-        json={"msg": msg, "api_key": api_key},
+        json={"msg": req.msg, "api_key": req.api_key},
     )
     return res.json()
