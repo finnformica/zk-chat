6795c4486858b1acfeb45c622eded82bbc9c5bbf
finnformica <finnformica@gmail.com>
2023-07-27 09:10:07 +0700

    add timeout flag

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn proxy_server:app -w 4 -k uvicorn.workers.UvicornWorker --reload -b 0.0.0.0:10000
+gunicorn proxy_server:app -w 4 -k uvicorn.workers.UvicornWorker --reload -b 0.0.0.0:10000 --timeout 0
