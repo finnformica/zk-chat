d469d8d43184e2ea36195dc85ee9158319707721
finnformica <finnformica@gmail.com>
2023-08-04 11:32:42 +0700

    update sys path for app imports

--- tests/unit/test_tps.py
+++ tests/unit/test_tps.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname("zk-chat")))
+sys.path.append(os.path.dirname("zk-chat"))
 from third_party_server import app
 
 
