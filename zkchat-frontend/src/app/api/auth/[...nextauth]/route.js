ca31bdd5311d3b0a4885767d0f0b49ceb6fb188b
finnformica <finnformica@gmail.com>
2023-08-29 12:14:04 +0700

    remove Google IdP

--- zkchat-frontend/src/app/api/auth/[...nextauth]/route.js
+++ zkchat-frontend/src/app/api/auth/[...nextauth]/route.js
@@ -15,10 +15,10 @@ export const authOptions = {
         },
       },
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
 };
